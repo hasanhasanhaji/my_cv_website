54ef5c79c38d7b811479d8594a1d855a0ca9c6b6
sorenagorgan <hasanhasanhaji@gmail.com>
2023-12-22 15:57:58 +0330

    some minor changes
    ++

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def blog_view(request):
+    return render(request, 'blog/blog.html')
+
+
+def blog_single(request):
+    return render(request, 'blog/single-blog.html')
--- website/urls.py
+++ website/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cvweb.urls'))
+    path('', include('cvweb.urls')),
+    path('blog/', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
