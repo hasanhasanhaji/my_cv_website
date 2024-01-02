e497eac5adf99f1d0a704f6843b1a22021bb0f1e
sorenagorgan <hasanhasanhaji@gmail.com>
2024-01-02 09:47:23 +0330

    Create model class
    Create model class for this project

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from blog.models import Post, Category
+
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'counted_views', 'status', 'published_date')
+    date_hierarchy = 'created_date'  # let us see our object in admin panel based on date
+    empty_value_display = 'null'
+    list_filter = ('status', 'author')
+    search_fields = ['title', 'content']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Category(models.Model):  # create class for tags
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='blog/', default='blog/def.jpg')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255, default='')
+    content = models.TextField()
+    tag = models.ManyToManyField(Category, )
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=True)
+    published_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):  # a function that shows return value in admin page instead of object
+        return self.title
